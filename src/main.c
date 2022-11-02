1cc0889f6f6e0ff1660e52a74c66a422d2780183
Kah Hou Sim <kahhousim@gmail.com>
2022-11-03 00:22:32 +0100

    Patched the CMakeLists.txt(updated), modified main.c for coding challenge II

--- src/main.c
+++ src/main.c
@@ -2,7 +2,25 @@
 
 int main(void)
 {
-	printf("Hello ESPL");
+	unsigned int number = 0;
+	char* word = NULL;
+	char decision = 'N';
+	
+	do
+	{
+		printf("Please enter a number between 0 and 9, then press ENTER\n");
+		scanf("%d",&number);
+		fflush(stdin);
+
+		word = num_to_words(number);
+
+		printf("%d : %s \n",&number, word);
+
+		printf("Do you wish to exit ? Y/N\n");
+		scanf("%c",&decision);
+		fflush(stdin);
+	}while(decision != 'Y');
 
 	return 0;
+		
 }
